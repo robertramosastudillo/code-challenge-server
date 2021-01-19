2f2267e674c32291c687d20d75f1c41fa4de077c
Robert Andres Ramos Astudillo <rramos.playtechla@gmail.com>
2021-01-19 17:26:41 -0500

    Validation of whether a text is palindrome was performed

--- domain/text/TextController.js
+++ domain/text/TextController.js
@@ -1,14 +1,21 @@
-const { reverseString } = require("../../utils/CustomMethods");
+const { reverseString, checkPalydrome } = require("../../utils/CustomMethods");
 
 // GetReverseText
 exports.getReverseText = (req, res) => {
-
   // Validar si existe la variable text
-  const { text } = req.query;
+  let { text } = req.query;
 
+  // We convert the text to lowercase
+  text = text.toLowerCase();
+
+  // Check if it is palindrome
+  const isPalindrome = checkPalydrome(text);
   //   The method is called to invert the string
   const invertedText = reverseString(text);
 
   //   We return the result of the inverted text
-  return res.send(invertedText);
+  return res.status(200).json({
+    text: invertedText,
+    palindrome: isPalindrome,
+  });
 };
--- utils/CustomMethods.js
+++ utils/CustomMethods.js
@@ -6,3 +6,16 @@ exports.reverseString = (text) => {
 
   return newText;
 };
+
+exports.checkPalydrome = (text) => {
+
+  // eliminamos los espacios en blanco
+  text = text.replace(/ /g, "");
+
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] != text[text.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
