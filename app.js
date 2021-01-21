e5fd8a1166c784082b39ae62196c8ad79f87b051
Robert Andres Ramos Astudillo <rramos.playtechla@gmail.com>
2021-01-20 20:24:25 -0500

    Frontend dominance was implemented in cors

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const cors = require("cors");
 const app = express();
 
 // Enable cors
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+};
+app.use(cors(corsOptions));
 
 // Enable express.json
 app.use(express.json());
